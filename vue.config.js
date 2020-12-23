397f4d356322d0020c2f493579585278084f4748
Ahmad Fajar <ahmad.fajar234@yahoo.com>
2020-12-24 00:39:56 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
     productionSourceMap: false,
     parallel: false,
-    publicPath: process.env.NODE_ENV === 'production' ? '/mdbootstrap-demo/' : '/',
+    publicPath: '/', // process.env.NODE_ENV === 'production' ? '/mdbootstrap-demo/' : '/',
     configureWebpack: {
         plugins: [
             new webpack.IgnorePlugin({
